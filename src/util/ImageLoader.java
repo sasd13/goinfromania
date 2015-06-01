348b3055a5432ee681cbd1e5178cf3dc4544295f
sasd13 <samir_saidali@yahoo.fr>
2015-06-02 01:08:00 +0200

    Optimizations

--- src/util/ImageLoader.java
+++ src/util/ImageLoader.java
@@ -3,20 +3,26 @@ package util;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
 
 import javax.imageio.ImageIO;
 
+import main.Main;
+
 public class ImageLoader {
 	
-	private static final String IMAGE_DIR = "img/";
+	private static final String IMAGE_DIR = "/";
 
 	public static BufferedImage loadFromPath(String fileName) {
 		BufferedImage bufferedImage = null;
 		
-		File file = new File(IMAGE_DIR + fileName);
+		URL url = Main.class.getResource(IMAGE_DIR + fileName);
+		
 		try {
+			File file = new File(url.toURI());
 			bufferedImage = ImageIO.read(file);
-		} catch (IOException e) {
+		} catch (URISyntaxException | IOException e ) {
 			e.printStackTrace();
 		}
 		
